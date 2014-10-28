10e0087930986b52b53f4d8de9e26f8708ab6468
Sigve Dreyer <sigve.dreyer@steria.no>
2014-10-28 13:32:10 +0100

    more

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/App.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World2!" );
+        System.out.println( "Hello World23!" );
+    }
+    
+    public static boolean alwaysTrue(){
+        return true;
     }
 }
